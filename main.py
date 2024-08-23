a5d4c69e867ea9cf453606a8386a5dc6ab035d47
Kaustubh Solanki <kaustubh@technokart.in>
2024-08-23 10:36:48 +0000

    latest session

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from PIL import Image
 from io import BytesIO
 import os
 import cv2
-from flask import Flask, request
+from flask import Flask, request, session
 from werkzeug.utils import secure_filename
 import shutil
 import csv
 import datetime
 from flask_cors import CORS, cross_origin
+from flask_session import Session
+import json
 
 uploads_dir = 'uploads'
 attemps_dir = 'attempts'
@@ -53,103 +55,118 @@ def get_health_plans(plan):
             remaining_health_plans.append(i)
     return remaining_health_plans
 
+@app.route('/login', methods = ['POST'])
+@cross_origin()
+def getDetails():
+    loginData = request.get_json()
+    print(loginData["username"], loginData['password'])
+    if(loginData['username'] == "Aubank" and loginData['password'] == "Aubank@123"):
+      print("here")
+      session['username'] = "AUBankDemo"
+      return {"status": "true", "message": "Login successful"}
+    else:
+      return {"status": "false", "message": "Invalid credentials"}
+
 @app.route('/insurance', methods = ['POST'])
 @cross_origin()
 def get_health():
-    if os.path.exists('yolov5/runs/detect/insurance_plans/'):
+      if os.path.exists('yolov5/runs/detect/insurance_plans/'):
 
-        now = datetime.datetime.now()
-        formatted_datetime = now.strftime("%Y_%m_%d_%H_%M_%S")
+          now = datetime.datetime.now()
+          formatted_datetime = now.strftime("%Y_%m_%d_%H_%M_%S")
 
-        shutil.move('yolov5/runs/detect/insurance_plans/', attemps_dir)
-        # renaming 
-        current_dir_path = 'attempts/insurance_plans'
-        new_dir_path = 'attempts/insurance_plans' + '_' + formatted_datetime
-        os.rename(current_dir_path, new_dir_path)
+          shutil.move('yolov5/runs/detect/insurance_plans/', attemps_dir)
+          # renaming 
+          current_dir_path = 'attempts/insurance_plans'
+          new_dir_path = 'attempts/insurance_plans' + '_' + formatted_datetime
+          os.rename(current_dir_path, new_dir_path)
 
-    if request.method == 'POST':
-        ## -------------------- for base64 as input
-        # data = request.get_json()
-        # base64_string = data['data']
-        # image_bytes = base64_to_image(base64_string)
-        # img = create_image_from_bytes(image_bytes)
-        # print("1")
+      if request.method == 'POST':
+          ## -------------------- for base64 as input
+          # data = request.get_json()
+          # base64_string = data['data']
+          # image_bytes = base64_to_image(base64_string)
+          # img = create_image_from_bytes(image_bytes)
+          # print("1")
 
-        # now = datetime.datetime.now()
-        # formatted_datetime = now.strftime("%Y_%m_%d_%H_%M_%S")
-        
-        # filename_1 = 'image_' + formatted_datetime + ".png"
-  
-        # img.save(os.path.join(uploads_dir, filename_1))
-        # file_path = os.path.join(uploads_dir, filename_1)
+          # now = datetime.datetime.now()
+          # formatted_datetime = now.strftime("%Y_%m_%d_%H_%M_%S")
+          
+          # filename_1 = 'image_' + formatted_datetime + ".png"
+    
+          # img.save(os.path.join(uploads_dir, filename_1))
+          # file_path = os.path.join(uploads_dir, filename_1)
 
-        ## --------------- for file as input
-        file = request.files["file"]
+          ## --------------- for file as input
+          file = request.files["file"]
 
-        now = datetime.datetime.now()
-        formatted_datetime = now.strftime("%Y_%m_%d_%H_%M_%S")
+          now = datetime.datetime.now()
+          formatted_datetime = now.strftime("%Y_%m_%d_%H_%M_%S")
 
-        filename = secure_filename(file.filename)
+          filename = secure_filename(file.filename)
 
-        file_name = os.path.splitext(filename)[0]
-        file_extension = os.path.splitext(filename)[1]
-        
-        filename = 'image_' + formatted_datetime + file_extension
+          file_name = os.path.splitext(filename)[0]
+          file_extension = os.path.splitext(filename)[1]
+          
+          filename = 'image_' + formatted_datetime + file_extension
 
-        file.save(os.path.join(uploads_dir, filename))
-        file_path = os.path.join(uploads_dir, filename)
+          file.save(os.path.join(uploads_dir, filename))
+          file_path = os.path.join(uploads_dir, filename)
 
 
 
-        print(f'################ file path = {file_path}')
-        
-        result = os.system(f'python yolov5/detect.py --weights best.pt --img 416 --conf 0.1 --source {file_path} --save-csv --name insurance_plans')
-        term_file_path = 'yolov5/runs/detect/insurance_plans/predictions.csv'
-        try : 
-            lines_list = []
-            with open(term_file_path, mode ='r') as file:
-                csvFile = csv.reader(file)
+          print(f'################ file path = {file_path}')
+          
+          result = os.system(f'python yolov5/detect.py --weights best.pt --img 416 --conf 0.1 --source {file_path} --save-csv --name insurance_plans')
+          term_file_path = 'yolov5/runs/detect/insurance_plans/predictions.csv'
+          try : 
+              lines_list = []
+              with open(term_file_path, mode ='r') as file:
+                  csvFile = csv.reader(file)
 
-                for lines in csvFile:
-                    lines_list.append(lines)
-            print(lines_list[0][1])
+                  for lines in csvFile:
+                      lines_list.append(lines)
+              print(lines_list[0][1])
 
-            label = lines_list[0][1]
-            health_plan_bool = 0
-            term_plan_bool = 0
-            if label in health_plans:
-                health_plan_bool = 1
-                remaining_plans = get_health_plans(label)
-            elif label in term_plans:
-              term_plan_bool = 1
-              remaining_plans = get_term_plans(label)
-            else:
-                remaining_plans = ['aditya_birla_digishield_plans','bajaj_alliance_life_etouch', 'hdfc_click2protect_super','icici_iprotect_smart', 'max_life_smart_secure_plus', 'aditya_birla_activ_health' , 'bajaj_alliance_health_guard', 'hdfc_ergo' , 'icici_lombard', 'tata_aig']
+              label = lines_list[0][1]
+              health_plan_bool = 0
+              term_plan_bool = 0
+              if label in health_plans:
+                  health_plan_bool = 1
+                  remaining_plans = get_health_plans(label)
+              elif label in term_plans:
+                term_plan_bool = 1
+                remaining_plans = get_term_plans(label)
+              else:
+                  remaining_plans = ['aditya_birla_digishield_plans','bajaj_alliance_life_etouch', 'hdfc_click2protect_super','icici_iprotect_smart', 'max_life_smart_secure_plus', 'aditya_birla_activ_health' , 'bajaj_alliance_health_guard', 'hdfc_ergo' , 'icici_lombard', 'tata_aig']
 
-            output_json['logo'] = label
-            output_json['error'] = "NA"
-            output_json['recommendations'] = remaining_plans
-            output_json['health_plan_bool'] = health_plan_bool
-            output_json['term_plan_bool'] = term_plan_bool
-            
-        except : 
-            print("no detections were made")
-            output_json['logo'] = "NA"
-            output_json['error'] = "no detections were made."
-            output_json['recommendations'] = ['aditya_birla_digishield_plans','bajaj_alliance_life_etouch', 'hdfc_click2protect_super','icici_iprotect_smart', 'max_life_smart_secure_plus', 'aditya_birla_activ_health' , 'bajaj_alliance_health_guard', 'hdfc_ergo' , 'icici_lombard', 'tata_aig']
-            
-            
-        finally : 
-            shutil.move('yolov5/runs/detect/insurance_plans/', attemps_dir)
+              output_json['logo'] = label
+              output_json['error'] = "NA"
+              output_json['recommendations'] = remaining_plans
+              output_json['health_plan_bool'] = health_plan_bool
+              output_json['term_plan_bool'] = term_plan_bool
+              
+          except : 
+              print("no detections were made")
+              output_json['logo'] = "NA"
+              output_json['error'] = "no detections were made."
+              output_json['recommendations'] = ['aditya_birla_digishield_plans','bajaj_alliance_life_etouch', 'hdfc_click2protect_super','icici_iprotect_smart', 'max_life_smart_secure_plus', 'aditya_birla_activ_health' , 'bajaj_alliance_health_guard', 'hdfc_ergo' , 'icici_lombard', 'tata_aig']
+              
+              
+          finally : 
+              shutil.move('yolov5/runs/detect/insurance_plans/', attemps_dir)
 
-            # renaming 
-            current_dir_path = 'attempts/insurance_plans'
-            new_dir_path = 'attempts/insurance_plans' + '_' + formatted_datetime
-            os.rename(current_dir_path, new_dir_path)
-    return output_json
+              # renaming 
+              current_dir_path = 'attempts/insurance_plans'
+              new_dir_path = 'attempts/insurance_plans' + '_' + formatted_datetime
+              os.rename(current_dir_path, new_dir_path)
+      return output_json
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
+    app.run(host="0.0.0.0",debug=True)
 
 '''
 sample response : 
--- ui/script.js
+++ ui/script.js
@@ -7,25 +7,27 @@ async function previewImage(event) {
   uploadedFile = file;
   const preview = document.getElementById('image-preview');
   const fileUpload = document.getElementById('upload-file');
-  const noSelect = document.getElementById('no-select');
+  // const noSelect = document.getElementById('no-select');
   const scanningLine = document.getElementById('scanning-line');
   const overlay = document.getElementById('overlay');
 
   if (file) {
     const reader = new FileReader();
-    reader.onload = function (e) {
+    reader.onload = async function (e) {
       preview.src = e.target.result;
       preview.style.display = 'block';
       fileUpload.style.display = "none";
-      noSelect.style.display = "none";
+      // noSelect.style.display = "none";
       scanningLine.style.display = 'block';
       overlay.style.display = 'flex';
     }
     reader.readAsDataURL(file);
+    await callData()
   }
 }
 
 async function callData(event) {
+  document.getElementById('no_detected').style.display = "none";
   const scanningLine = document.getElementById('scanning-line');
   const overlay = document.getElementById('overlay');
   let term_plans = ['aditya_birla_digishield_plans', 'bajaj_alliance_life_etouch', 'hdfc_click2protect_super', 'icici_iprotect_smart', 'max_life_smart_secure_plus']
@@ -33,7 +35,7 @@ async function callData(event) {
   try {
     const formData = new FormData();
     formData.append('file', uploadedFile);
-    const response = await fetch('http://localhost:5000/insurance', {
+    const response = await fetch('http://74.225.136.211:5000/insurance', {
       method: 'POST',
       body: formData
     });
@@ -41,12 +43,15 @@ async function callData(event) {
     console.log('Success:', data);
     scanningLine.style.display = 'none';
     overlay.style.display = 'none';
-    document.getElementById(data.logo + '_detected').style.display = "flex";
-    document.getElementById('hr_tag').style.display = "block";
-    document.getElementById('detected').style.display = "flex";
-    if (data.term_plan_bool == 1) {
+    if (data.error == 'no detections were made.') {
+      console.log("HERE");
+      document.getElementById('no_detected').style.display = "flex";
+    } else if (data.term_plan_bool == 1 && data.logo != 'NA') {
       let index = term_plans.indexOf(data.logo);
-
+      document.getElementById(data.logo + '_detected').style.display = "flex";
+      document.getElementById('hr_tag').style.display = "block";
+      document.getElementById('suggestions').style.display = "flex";
+      document.getElementById('detected').style.display = "flex";
       if (index !== -1) {
         term_plans.splice(index, 1);
       }
@@ -54,9 +59,12 @@ async function callData(event) {
       for (let i of term_plans) {
         document.getElementById(i).style.display = "flex"
       }
-    } else if (data.health_plan_bool == 1) {
+    } else if (data.health_plan_bool == 1 && data.logo != 'NA') {
       let index = health_plans.indexOf(data.logo);
-
+      document.getElementById(data.logo + '_detected').style.display = "flex";
+      document.getElementById('hr_tag').style.display = "block";
+      document.getElementById('suggestions').style.display = "flex";
+      document.getElementById('detected').style.display = "flex";
       if (index !== -1) {
         health_plans.splice(index, 1);
       }
@@ -65,6 +73,32 @@ async function callData(event) {
 
         document.getElementById(i).style.display = "flex"
       }
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function login() {
+  try {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    const response = await fetch('http://74.225.136.211:5000/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    });
+    const data = await response.json();
+    console.log('Success:', data);
+    if (data.status == 'false') {
+      document.getElementById('wrong_creds').style.display = "block";
+    } else {
+      window.location.href = "http://74.225.136.211/main.html";
     }
   } catch (error) {
     console.error('Error:', error);
